20856cdf7b0a1cd017b8e1e1cdd9220161f1905c
Wechuli Simiyu <wechuli017@gmail.com>
2024-12-17 16:41:20 +0300

    add adonis mail using mailgun/mailjet and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.edge",
+    "./resources/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
