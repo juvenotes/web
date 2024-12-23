0520ad2572b86cd4eda01ebbcf56536831f58fef
Wechuli Simiyu <wechuli017@gmail.com>
2024-12-23 18:35:05 +0300

    add shadcn with button

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
 import mdx from '@mdx-js/rollup'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [
@@ -17,14 +20,22 @@ export default defineConfig({
     react(),
     adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
 
   /**
    * Define aliases for importing modules from
    * your frontend code
+   * for example
+   * import Header from `~/components/Button.tsx`
    */
   resolve: {
     alias: {
       '~/': `${getDirname(import.meta.url)}/inertia/`,
+      '@': path.resolve(import.meta.dirname, './inertia'),
     },
   },
 })
