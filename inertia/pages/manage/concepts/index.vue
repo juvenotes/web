8437f09ffb51458caf3dbc06d1d8e70d5436a290
Wechuli Simiyu <wechuli017@gmail.com>
2025-01-09 23:59:36 +0300

    render markdown content directly from database

--- inertia/pages/manage/concepts/index.vue
+++ inertia/pages/manage/concepts/index.vue
@@ -13,6 +13,7 @@ defineProps<Props>()
 </script>
 
 <template>
+  <AppHead title="Manage concepts" description="Manage concepts in Juvenotes" />
   <div class="container mx-auto px-4 py-8">
     <div class="flex justify-between items-center mb-6">
       <h1 class="text-2xl font-bold">Medical Concepts</h1>
