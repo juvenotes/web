8d3d0a44749769b7609e24ac7e6a5590d1d1e3fc
Wechuli Simiyu <wechuli017@gmail.com>
2025-01-12 19:54:00 +0300

    add default email flow

--- config/mail.ts
+++ config/mail.ts
@@ -2,7 +2,12 @@ import env from '#start/env'
 import { defineConfig, transports } from '@adonisjs/mail'
 
 const mailConfig = defineConfig({
-  default: 'brevo',
+  default: 'smtp',
+
+  from: {
+    address: 'admin@example.com',
+    name: 'Juvenotes Admin Team',
+  },
 
   /**
    * The mailers object can be used to configure multiple mailers
@@ -18,6 +23,15 @@ const mailConfig = defineConfig({
     brevo: transports.brevo({
       key: env.get('BREVO_API_KEY'),
       baseUrl: 'https://api.brevo.com/v3',
+    }),
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST'),
+      port: env.get('SMTP_PORT'),
+      auth: {
+        type: 'login',
+        user: env.get('SMTP_USERNAME'),
+        pass: env.get('SMTP_PASSWORD'),
+      },
     }),
   },
 })
--- start/env.ts
+++ start/env.ts
@@ -60,4 +60,14 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
   BREVO_API_KEY: Env.schema.string(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring the mail package
+  |----------------------------------------------------------
+  */
+  SMTP_HOST: Env.schema.string(),
+  SMTP_PORT: Env.schema.string(),
+  SMTP_USERNAME: Env.schema.string(),
+  SMTP_PASSWORD: Env.schema.string(),
 })
