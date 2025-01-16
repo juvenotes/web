d1e1453298d6ee238534e21b0969d653738d35dd
Wechuli Simiyu <wechuli017@gmail.com>
2025-01-17 00:34:41 +0300

    decision matrix for admin users complete

--- app/utils/ensure_admin.ts
+++ app/utils/ensure_admin.ts
@@ -1,8 +1,12 @@
 import db from '@adonisjs/lucid/services/db'
 import { Role } from '#enums/roles'
 import hash from '@adonisjs/core/services/hash'
+import env from '#start/env'
 
 export async function ensureAdmin() {
+  if (env.get('NODE_ENV') === 'production') {
+    return null
+  }
   let admin = await db.from('users').where('role_id', Role.ADMIN).first()
 
   if (!admin) {
