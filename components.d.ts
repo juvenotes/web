d615bd101ba575f7a18b03e8eb0c5d054747949d
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2025-01-31 22:11:39 -0500

    modified support page, pending privacy policy router

--- components.d.ts
+++ components.d.ts
@@ -7,10 +7,10 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AdminSearch: typeof import('./inertia/components/AdminSearch.vue')['default']
     AddMcqDialog: typeof import('./inertia/components/AddMcqDialog.vue')['default']
     AddQuestionDialog: typeof import('./inertia/components/AddQuestionDialog.vue')['default']
     AddSaqDialog: typeof import('./inertia/components/AddSaqDialog.vue')['default']
+    AdminSearch: typeof import('./inertia/components/AdminSearch.vue')['default']
     Alert: typeof import('./inertia/components/ui/alert/Alert.vue')['default']
     AlertDescription: typeof import('./inertia/components/ui/alert/AlertDescription.vue')['default']
     AlertTitle: typeof import('./inertia/components/ui/alert/AlertTitle.vue')['default']
@@ -65,7 +65,6 @@ declare module 'vue' {
     ToastProvider: typeof import('./inertia/components/ui/toast/ToastProvider.vue')['default']
     ToastTitle: typeof import('./inertia/components/ui/toast/ToastTitle.vue')['default']
     ToastViewport: typeof import('./inertia/components/ui/toast/ToastViewport.vue')['default']
-    Toggle: typeof import('./inertia/components/ToggleUrl.vue')['default']
     ToggleUrl: typeof import('./inertia/components/ToggleUrl.vue')['default']
     UploadMcqsDialog: typeof import('./inertia/components/UploadMcqsDialog.vue')['default']
     UploadQuestionsDialog: typeof import('./inertia/components/UploadQuestionsDialog.vue')['default']
