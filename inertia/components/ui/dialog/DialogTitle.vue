0edfc7f9dfa9e363decf5c52d20cdcf7b880e834
Wechuli Simiyu <wechuli017@gmail.com>
2025-02-07 11:58:23 +0300

    add textarea instead of input in expected answer to display lists

--- inertia/components/ui/dialog/DialogTitle.vue
+++ inertia/components/ui/dialog/DialogTitle.vue
@@ -17,7 +17,12 @@ const forwardedProps = useForwardProps(delegatedProps)
 <template>
   <DialogTitle
     v-bind="forwardedProps"
-    :class="cn('text-lg font-semibold leading-none tracking-tight', props.class)"
+    :class="
+      cn(
+        'text-lg font-semibold leading-none tracking-tight',
+        props.class,
+      )
+    "
   >
     <slot />
   </DialogTitle>
